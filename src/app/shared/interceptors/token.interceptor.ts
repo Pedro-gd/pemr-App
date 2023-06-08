f1930b15d45a87dfc299c0afbfad5c9b1ab65b93
Pedro-gd <80784527+Pedro-gd@users.noreply.github.com>
2023-06-08 11:31:26 -0600

    2 files modified, 2 deleted

--- src/app/shared/interceptors/token.interceptor.ts
+++ src/app/shared/interceptors/token.interceptor.ts
@@ -9,24 +9,6 @@ import {
 import { Observable } from 'rxjs';
 
 @Injectable()
-export class TokenInterceptor implements HttpInterceptor {
-  constructor(private authSvc: AuthService) { }
-
-  intercept(
-    request: HttpRequest<unknown>,
-    next: HttpHandler
-  ): Observable<HttpEvent<any>> {
-    if (request.headers.get('require-token')) {
-      const token = this.authSvc.tokenValue;
-      if (token) {
-        const authReq = request.clone({
-          setHeaders: {
-            Authorization: ` Bearer ${token}`,
-          },
-        });
-        return next.handle(authReq);
-      }
-    }
-    return next.handle(request);
-  }
+export class TokenInterceptor {
+  constructor() {}
 }
